05a39692a9b14deaee1cddbfbb082eba549d3397
Adrian Wobito <adrian.wobito@gmail.com>
2019-01-31 10:39:35 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Build
+##Beanstalk Printer Queue Worker
+
+Copy .env.example as .env and if need be replace localhost with your Beanstalkd Server IP
+
+###Build Binary
+```
+cd src && go build -o ../bin/SeminarPrinter
+```
--- src/main.go
+++ src/main.go
@@ -1,11 +1,25 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	workerMain()
 }
 
 func workerMain() {
-	worker := makeNewWorker("localhost:11300", "printer")
+	beanstalkServerAddress := os.Getenv("BEANSTALK_SERVER")
+
+	worker := makeNewWorker(beanstalkServerAddress, "printer")
 	worker.Connect()
 	defer worker.Close()
 
